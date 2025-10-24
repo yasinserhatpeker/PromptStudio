3cc235aa6b7663668ab3cad95dd79ac77c7e1fff
Yasin <yasinpeker43@gmail.com>
2025-10-24 17:32:04 +0300

    interface outline is implemented

--- PromptStudio.Infrastructure/Services/PromptService.cs
+++ PromptStudio.Infrastructure/Services/PromptService.cs
@@ -1,14 +1,26 @@
 using System;
+using AutoMapper;
 using PromptStudio.Application.DTOs.Prompt;
 using PromptStudio.Application.Services.Prompts;
+using PromptStudio.Infrastructure.Data;
 
 namespace PromptStudio.Infrastructure.Services;
 
 public class PromptService : IPromptService
 {
+    private readonly PromptStudioDbContext _context;
+    private readonly IMapper _mapper;
+     public PromptService(PromptStudioDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    
     public Task<PromptResponseDTO> CreatePromptAsync(CreatePromptDTO createPromptDTO)
     {
-        throw new NotImplementedException();
+        // Mapping domainModel to DTO
+        
+        
     }
 
     public Task<bool> DeletePromptAsync(Guid id)
