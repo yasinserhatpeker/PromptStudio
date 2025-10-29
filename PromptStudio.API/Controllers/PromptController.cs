695511cea55abe86d7cfc5e772775f40954b40e4
Yasin <yasinpeker43@gmail.com>
2025-10-29 15:36:47 +0300

    add delete endpoint to promptController

--- PromptStudio.API/Controllers/PromptController.cs
+++ PromptStudio.API/Controllers/PromptController.cs
@@ -34,9 +34,9 @@ namespace PromptStudio.API.Controllers
 
         }
 
-       // GET api/prompt/{id}
+        // GET api/prompt/{id}
         [HttpGet("{id}")]
-       public async Task<IActionResult> GetPromptById(Guid id) 
+        public async Task<IActionResult> GetPromptById(Guid id)
         {
             var prompt = await _promptService.GetPromptByIdAsync(id);
             if (prompt == null)
@@ -45,6 +45,34 @@ namespace PromptStudio.API.Controllers
             }
             return Ok(prompt);
         }
+
+
+        // DELETE api/prompt/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePromptById(Guid userId, Guid id)
+        {
+            var prompt = await _promptService.GetPromptByIdAsync(id);
+            if (prompt == null)
+            {
+                return NotFound("prompt cannot found");
+            }
+            if (prompt.UserId != userId)
+            {
+                return Forbid();
+            }
+            var result = await _promptService.DeletePromptAsync(userId, id);
+            if (!result)
+            {
+                return StatusCode(500, "An error occured during the prompt deletion");
+            }
+            return NoContent();
+
+
+        }
+        
+        
+    
+        
 
         
     }
