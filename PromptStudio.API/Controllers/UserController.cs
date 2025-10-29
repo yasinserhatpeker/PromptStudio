1cf585b8f6d7b64d601c0af7481d9a02737f35df
Yasin <yasinpeker43@gmail.com>
2025-10-29 16:43:13 +0300

    DI for userController

--- PromptStudio.API/Controllers/UserController.cs
+++ PromptStudio.API/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PromptStudio.Application.Services.Users;
 
 namespace PromptStudio.API.Controllers
 {
@@ -7,5 +8,11 @@ namespace PromptStudio.API.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        
     }
 }
