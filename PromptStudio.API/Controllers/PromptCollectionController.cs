d0f81137c0517f0fae2a78bfbf30e4bc179a38b4
Yasin <yasinpeker43@gmail.com>
2025-10-30 11:09:30 +0300

    collectionCreate endpoint is done

--- PromptStudio.API/Controllers/PromptCollectionController.cs
+++ PromptStudio.API/Controllers/PromptCollectionController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PromptStudio.Application.DTOs.Collection;
 using PromptStudio.Application.Services.Collections;
 using PromptStudio.Application.Services.Prompts;
 
@@ -16,6 +17,25 @@ namespace PromptStudio.API.Controllers
             _collectionService = collectionService;
         }
 
+        // POST api/collection/
+        [HttpPost]
+        public async Task<IActionResult> CreatePromptCollection([FromBody] CreateCollectionDTO createCollectionDTO, [FromRoute] Guid userId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var promptCollection = await _collectionService.CreatePromptCollectionAsync(createCollectionDTO, userId);
+
+            if (promptCollection == null)
+            {
+                return BadRequest("creating collection is failed");
+            }
+
+            return CreatedAtAction(nameof(GetPromptCollectionsByUser), new { userId = userId }, promptCollection);
+
+        }
         
+
     }
 }
