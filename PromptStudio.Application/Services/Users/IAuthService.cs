cf755222f56d0a4259d928e2cb74141fdad1b711
Yasin <yasinpeker43@gmail.com>
2025-11-15 11:49:54 +0300

    AuthService signature is changed for authResultDTO

--- PromptStudio.Application/Services/Users/IAuthService.cs
+++ PromptStudio.Application/Services/Users/IAuthService.cs
@@ -6,8 +6,8 @@ namespace PromptStudio.Application.Services.Users;
 
 public interface IAuthService
 {
-    Task<string?> LoginAsync(LoginDTO loginDTO);
+    Task<AuthResultDTO?> LoginAsync(LoginDTO loginDTO);
     Task<UserResponseDTO?> RegisterAsync(CreateUserDTO createUserDTO);
-    Task<string?> RefreshTokenAsync(string refreshToken);   
+    Task<AuthResultDTO?> RefreshTokenAsync(string refreshToken);
 
  }
